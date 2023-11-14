1c6296af1337ffec4d9117ea764981accdad1236
peter <peter@gmail.com>
2023-11-14 07:05:46 -0800

    added peter file

--- app.js
+++ app.js
@@ -8,9 +8,20 @@ const app = express();
 const PORT = 5000;
 
 app.use(morgan("dev"));
+app.set("view engine", "ejs")
+// app.get("/", function (req, res) {
+//   res.send("OUR HOMEPAGE");
+// });
 
-app.get("/", function (req, res) {
-  res.send("OUR HOMEPAGE");
-});
+
+app.get('/', (req, res) =>{
+  var student = [
+    { Name: 'cindy', email: 'cindy@gmail.com'},
+    { Name: 'peter', email: 'peter@gmail.com'}
+  ]
+  res.render('pages/index', {
+    students : student
+  })
+})
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
