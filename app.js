b43eb53db809b44fcd55256155529ba80b7e0794
Cindylov <cynthiaujunwa4@gmail.com>
2023-12-06 15:28:41 +0100

    cindy

--- app.js
+++ app.js
@@ -8,9 +8,34 @@ const app = express();
 const PORT = 5000;
 
 app.use(morgan("dev"));
-
+app.set("view engine", "ejs");
+// app.get("/", function (req, res) {
+//   res.send("OUR HOMEPAGE");
+// });
 app.get("/", function (req, res) {
-  res.send("OUR HOMEPAGE");
+  var student = [
+    { name: "Cindy", email: "cindy@gmail.com" },
+    { name: "Peter", email: "peter@gmail.com" },
+  ];
+  res.render("pages/index", {
+    students: student,
+  });
+});
+
+app.get("/About", function (req, res) {
+  res.render("pages/About");
+});
+
+app.get("/contact", function (req, res) {
+  res.render("pages/contact");
+});
+
+app.get("/login", function (req, res) {
+  res.render("pages/login");
+});
+
+app.get("/sigin", function (req, res) {
+  res.render("pages/sigin");
 });
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
